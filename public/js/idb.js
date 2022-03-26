e292070425a49a1258c22b66e1d0ff9e9647f0b7
kelsey-schenk <kelsey.schenk@att.net>
2022-03-26 14:39:40 -0400

    save function

--- public/js/idb.js
+++ public/js/idb.js
@@ -6,3 +6,22 @@ request.onupgradeneeded = function (event) {
     const db = event.target.result;
     db.createObjectStore('new_budget', { autoIncrement: true });
 };
+
+request.onsuccess = function (event) {
+    db = event.target.result;
+    if (navigator.onLine) {
+        uploadBudget();
+    }
+};
+
+request.onerror = function (event) {
+    console.log("Error: " + event.target.errorCode);
+}
+
+function saveRecord(record) {
+    const transaction = db.transaction(["new_budget"], "readwrite");
+
+    const store = transaction.objectStore("new_budget");
+
+    store.add(record);
+}
