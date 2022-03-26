8f731d8b6b5a2084f4858657ed5bee52687dd81a
kelsey-schenk <kelsey.schenk@att.net>
2022-03-26 14:28:34 -0400

    finished server file

--- server.js
+++ server.js
@@ -2,5 +2,28 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
+const { allowedNodeEnvironmentFlags } = require("process");
 
 const PORT = process.env.PORT || 3001;
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(compresion());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(MONGODB_URI, {
+    useNewURlParser: true,
+    useFindAndModify: false
+});
+
+app.use(require("./routes/api.js"));
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+});
